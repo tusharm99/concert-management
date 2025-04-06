0e6c9416d71ced67c76829aa1910ac737ebb453a
tushar mehere <tushar.mehere@gmail.com>
2025-04-05 22:47:47 -0700

    adding events controller

---
+++ ConcertManagement/ConcertManagement/Controllers/EventsController.cs
@@ -0,0 +1,52 @@
+﻿using ConcertManagement.Core.Dtos;
+using ConcertManagement.Service;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ConcertManagement.Api.Controllers
+{
+    [Route("api/events")]
+    [ApiController]
+    public class EventsController : ControllerBase
+    {
+        private readonly ILogger<EventsController> _logger;
+        private readonly IConcertService _concertService;
+
+        public EventsController(ILogger<EventsController> logger, IConcertService concertService)
+        {
+            _logger = logger;
+            _concertService = concertService;
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetEventById(int id)
+        {
+            _logger.LogInformation("Fetching events for eventId {id}", id);
+            var result = await _concertService.GetEvent(id);
+            return result != null ? Ok(result) : NotFound();
+        }
+
+
+        [HttpGet(Name = "venue/{venueId}")]
+        public async Task<IActionResult> GetEventsByVenue(int venueId)
+        {
+            _logger.LogInformation("Fetching events against venueId {VenueId}", venueId);
+            var events = await _concertService.GetEventsByVenue(venueId);
+            return Ok(events);
+        }
+
+        [HttpPost(Name = "")]
+        public async Task<IActionResult> AddEvent([FromBody] EventDto item)
+        {
+            if (item == null || !ModelState.IsValid)
+            {
+                _logger.LogWarning("Invalid event data");
+                return BadRequest(ModelState);
+            }
+
+            var createdEvent = await _concertService.CreateEvent(item);
+
+            _logger.LogInformation("Event created with ID: {EventId}", createdEvent.Id);
+            return CreatedAtAction(nameof(GetEventById), new { id = createdEvent.Id }, createdEvent);
+        }
+    }
+}
--- ConcertManagement/ConcertManagement/Program.cs
+++ ConcertManagement/ConcertManagement/Program.cs
@@ -1,20 +1,51 @@
+using ConcertManagement.Data.Entities;
+using ConcertManagement.Data.Repositories;
+using ConcertManagement.Infrastructure;
+using ConcertManagement.Service;
+using Microsoft.EntityFrameworkCore;
+using Serilog;
 
-namespace ConcertManagement
+namespace ConcertManagement.Api
 {
     public class Program
     {
         public static void Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
+            var configuration = builder.Configuration;
 
             // Add services to the container.
+
+            builder.Services.AddDbContextPool<CmDbContext>(options =>
+            {
+                options.UseSqlServer(configuration.GetConnectionString("DbContext"),
+                provideroptions => provideroptions.EnableRetryOnFailure());
+            });
+
+            Log.Logger = new LoggerConfiguration()
+                                .ReadFrom.Configuration(builder.Configuration)
+                                .Enrich.FromLogContext()
+                                .WriteTo.File("Logs/log.txt", rollingInterval: RollingInterval.Day)
+                                .CreateLogger();
+
+            builder.Host.UseSerilog();
 
             builder.Services.AddControllers();
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
 
+            builder.Services.AddScoped(typeof(IGenericRepository<>), typeof(GenericRepository<>));
+            builder.Services.AddScoped<IVenuesRepository, VenuesRepository>();
+            builder.Services.AddScoped<IEventsRepository, EventsRepository>();
+            builder.Services.AddScoped<IReservationsRepository, ReservationsRepository>();
+            builder.Services.AddScoped<IConcertService, ConcertService>();
+
+            builder.Services.AddAutoMapper(typeof(AutoMapperProfile));
+
             var app = builder.Build();
+
+            app.UseSerilogRequestLogging(); // to log Http requests
 
             // Configure the HTTP request pipeline.
             if (app.Environment.IsDevelopment())
