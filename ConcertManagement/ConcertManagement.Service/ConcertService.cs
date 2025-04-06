70f996c6c89839624c551b44784deaf93fc657f5
tushar mehere <tushar.mehere@gmail.com>
2025-04-05 22:47:11 -0700

    adding service layer

---
+++ ConcertManagement/ConcertManagement.Service/ConcertService.cs
@@ -0,0 +1,55 @@
+﻿using AutoMapper;
+using ConcertManagement.Core.Dtos;
+using ConcertManagement.Core.Entities;
+using ConcertManagement.Data.Repositories;
+using Microsoft.Extensions.Logging;
+
+namespace ConcertManagement.Service
+{
+    public class ConcertService : IConcertService
+    {
+        private readonly IEventsRepository _eventsRepository;
+        private readonly IReservationsRepository _reservationRepository;
+        private readonly IVenuesRepository _venueRepository;
+        private readonly IMapper _mapper;
+        private readonly ILogger<ConcertService> _logger;
+
+        public ConcertService(IEventsRepository eventsRepository,
+                              IReservationsRepository reservationRepository,
+                              IVenuesRepository venueRepository,
+                              IMapper mapper,
+                              ILogger<ConcertService> logger)
+        {
+            _eventsRepository = eventsRepository;
+            _reservationRepository = reservationRepository;
+            _venueRepository = venueRepository;
+            _mapper = mapper;
+            _logger = logger;
+        }
+
+        public async Task<Event> GetEvent(int id)
+        {
+            return await _eventsRepository.GetByIdAsync(id).ConfigureAwait(false);
+        }
+
+        public async Task<IEnumerable<Event>> GetEventsByVenue(int venueId)
+        {
+            return await _eventsRepository.FindAsync(_ => _.Venue.Id == venueId).ConfigureAwait(false);
+        }
+
+        public async Task<Event> CreateEvent(EventDto item)
+        {
+            Event eventObj = null;
+            try
+            {
+                eventObj = _mapper.Map<Event>(item);
+            }
+            catch(Exception ex)
+            {
+                _logger.LogError(ex, $"Automapper Error: {ex.Message}");
+            }
+            return await _eventsRepository.AddAsync(eventObj).ConfigureAwait(false);
+        }
+
+    }
+}
---
+++ ConcertManagement/ConcertManagement.Service/IConcertService.cs
@@ -0,0 +1,12 @@
+﻿using ConcertManagement.Core.Dtos;
+using ConcertManagement.Core.Entities;
+
+namespace ConcertManagement.Service
+{
+    public interface IConcertService
+    {
+        Task<Event> GetEvent(int id);
+        Task<IEnumerable<Event>> GetEventsByVenue(int venueId);
+        Task<Event> CreateEvent(EventDto item);
+    }
+}
